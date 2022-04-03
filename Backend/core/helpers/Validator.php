1361bd930fea3f96a50e33f455d4c70efbaa363d
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-04-03 23:36:42 +0000

    Added More security to Date validation with REGEX to check format to "YYYY-MM-DD"

--- Backend/core/helpers/Validator.php
+++ Backend/core/helpers/Validator.php
@@ -331,6 +331,10 @@ class Validator
         if (!strtotime($value)) {
             return 'The value must be a date';
         }
+        // Check Date Format (YYYY-mm-dd)
+        if (!preg_match('/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/', $value)) {
+            return 'The value must be a date';
+        }
 
         return true;
     }
