bce83cfd985aec2f9411e489768469c9ea1a110a
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-04-05 23:00:59 +0000

    Using Dotenv package for loading environment variables

--- Backend/public/index.php
+++ Backend/public/index.php
@@ -1,8 +1,13 @@
 <?php
 
-require_once '../app/bootstrap.php';
-
 use Core\Router;
 use Core\Helpers\Request;
+use Dotenv\Dotenv;
+
+require_once '../vendor/autoload.php';
+
+// Load Environment Variables
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
 
 Router::load('../app/config/routes.php')->direct(new Request);
