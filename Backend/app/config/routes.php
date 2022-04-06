bf16d364df37772367a8a71903fb91c1fe671934
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-04-06 18:08:04 +0000

    Switched to using POST request instead of PUT and DELETE for more stability and to eleminate CORS error

--- Backend/app/config/routes.php
+++ Backend/app/config/routes.php
@@ -8,15 +8,15 @@ $router->post('api/login/admin', 'Auth@loginAdmin', ["Validation@username|passwo
 
 // Admin Dashboard Routes (JWT Token)
 $router->get('api/clients', 'Clients@index', ['Auth@admin']); //*🚀
-$router->get('api/client', 'Clients@show', ['Auth@admin','Validation@id']); //*🚀
-$router->post('api/client', 'Clients@store', ['Auth@admin','Validation@firstname|lastname|age|profession']); //*🚀
-$router->put('api/client', 'Clients@update', ['Auth@admin','Validation@id|firstname|lastname|age|profession']); //*🚀
-$router->delete('api/client', 'Clients@delete', ['Auth@admin','Validation@id']); //*
+$router->get('api/client', 'Clients@show', ['Auth@admin', 'Validation@id']); //*🚀
+$router->post('api/client', 'Clients@store', ['Auth@admin', 'Validation@firstname|lastname|age|profession']); //*🚀
+$router->post('api/client/update', 'Clients@update', ['Auth@admin', 'Validation@id|firstname|lastname|age|profession']); //*🚀
+$router->post('api/client/delete', 'Clients@delete', ['Auth@admin', 'Validation@id']); //*
 
 // Client Dashboard Routes (Header-based Api Key)
-$router->get('api/rdvs', 'RDV@index', ['Auth@client','Validation@client_id']); //*🚀
-$router->get('api/rdvs/timeslots', 'RDV@timeslots', ['Auth@client','Validation@date']); //*🚀
-$router->get('api/rdv', 'RDV@show', ['Auth@client','Validation@id']); //*🚀
-$router->post('api/rdv', 'RDV@store', ['Auth@client','Validation@client_id|date|time_slot|description']); //*🚀
-$router->put('api/rdv', 'RDV@update', ['Auth@client','Validation@id|client_id|date|time_slot|description']); //*🚀
-$router->delete('api/rdv', 'RDV@delete', ['Auth@client','Validation@id']); //*🚀
+$router->get('api/rdvs', 'RDV@index', ['Auth@client', 'Validation@client_id']); //*🚀
+$router->get('api/rdvs/timeslots', 'RDV@timeslots', ['Auth@client', 'Validation@date']); //*🚀
+$router->get('api/rdv', 'RDV@show', ['Auth@client', 'Validation@id']); //*🚀
+$router->post('api/rdv', 'RDV@store', ['Auth@client', 'Validation@client_id|date|time_slot|description']); //*🚀
+$router->post('api/rdv/update', 'RDV@update', ['Auth@client', 'Validation@id|client_id|date|time_slot|description']); //*🚀
+$router->post('api/rdv/delete', 'RDV@delete', ['Auth@client', 'Validation@id']); //*🚀
