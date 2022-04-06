dfb5cbe8c4753f8a408fd83d5ab649f666378904
TahaLechgar <tahamr08@gmail.com>
2022-04-06 17:28:00 +0000

    enhance rdv context

--- Frontend/src/contexts/rdvDatacontext.jsx
+++ Frontend/src/contexts/rdvDatacontext.jsx
@@ -1,4 +1,4 @@
-import React, {createContext, useState} from 'react';
+import React, {createContext, useMemo, useState} from 'react';
 
 
 
@@ -7,20 +7,32 @@ export const RDVContext = createContext(null);
 
 const RdvDataContext = ({children}) => {
 
-    const [rdvId, setRdvId]         = useState(0)
-    const [clientId, setClientId]   = useState(2)
-    const [date, setDate]           = useState('')
-    const [time, setTime]           = useState('')
-    const [description, setDescription]   = useState('')
+    const [state, setState] = useState({
+        rdvId: 0,
+        clientId: 2,
+        date: "",
+        time: 0,
+        description: "",
+    })
+    const [occupiedTimeSlots, setOccupiedTimeSlots]   = useState([])
 
+    const stateWithSetters = useMemo(() => {
+        const obj = {};
+        Object.keys(state).forEach(key => {
+            obj[key] = state[key];
+            const setterName = key[0].toUpperCase() + key.slice(1);
+            obj[`set${setterName}`] = (val) => {
+                setState(v => ({...v, [key]: typeof val === "function" ? val(v[key]) : val}))
+            }
+        })
+        return obj;
+    }, [state])
 
+    console.log(stateWithSetters);
     return (
         <RDVContext.Provider value={{
-            clientId, setClientId,
-            rdvId, setRdvId,
-            date, setDate,
-            time, setTime,
-            description, setDescription
+            ...stateWithSetters,
+            occupiedTimeSlots, setOccupiedTimeSlots
         }}>
             {children}
         </RDVContext.Provider>
